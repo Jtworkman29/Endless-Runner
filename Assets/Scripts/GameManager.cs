9bbd334029b995af21d84702e89896e7cf6a1402
Jtworkman29 <jtworkman@cia.edu>
2025-04-23 16:46:03 -0400

    SPrites and Menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -87,6 +88,7 @@ public class GameManager : MonoBehaviour
 
         activeObstacles.Clear();
         ResumeObstacles();
+        SceneManager.LoadScene(0);
     }
 
     public void PauseObstacles()
